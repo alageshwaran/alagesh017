bfd0910a27a221cdf93126535d47737b05b97130
alageshwaran <42169721+alageshwaran@users.noreply.github.com>
2019-03-07 11:00:26 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,1 +1,10 @@
 # alagesh017
+print ("enter '0' for exit.")
+ch=input ()
+if ch =='0':
+  exit()
+else:
+  if((ch>='a' and ch<='z')or(ch>='A' and ch<='Z')):
+    print("Alphabet")
+  else:
+    print("not an alphabet")
