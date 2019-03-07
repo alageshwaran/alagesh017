7d43715b1dbf632a4f0f055da91764e2c0078349
alageshwaran <42169721+alageshwaran@users.noreply.github.com>
2019-03-07 10:46:12 +0530

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,6 +1,6 @@
 # alagesh017
-ch=input("Enter the character:")
+ch=input()
 if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-  print(ch,"is vowel")
+  print("vowel")
 else:
-  print(ch,"invalid")
+  print("consonant")
