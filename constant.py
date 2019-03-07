830788e2626965b337f687afc2aaba0cb016c2ad
alageshwaran <42169721+alageshwaran@users.noreply.github.com>
2019-03-07 10:47:22 +0530

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,6 +1,6 @@
 # alagesh017
 ch=input()
 if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-  print("vowel")
+  print("Vowel")
 else:
-  print("consonant")
+  print("Consonant")
