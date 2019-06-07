c009654a1952450160f8a31f6d97f08d5293d88e
alageshwaran <42169721+alageshwaran@users.noreply.github.com>
2019-06-07 09:44:05 +0530

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,6 +1,7 @@
-# alagesh017
-ch=input()
-if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-  print("Vowel")
-else:
-  print("Consonant")
+n=input()
+n=ord(n)
+if ((n>=65&n<=90)or(n>=97&n<=122)):
+    if n==65 or n==69 or n==73 or n==79 or n==85 or n==97 or n==101 or n==105 or n==111 or n==117:
+        print('Vowel')
+    else:print('Consonant') 
+else:print('Invalid')
